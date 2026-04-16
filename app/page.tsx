9090259892c8e7fd419013ab80582e469a86564b
Samoolino <samo4au@gmail.com>
2026-04-16 18:52:17 +0200

    Create main portfolio homepage component

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+// This is the main portfolio homepage component
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Welcome to My Portfolio</h1>
+            <p>This is the homepage of my portfolio.</p>
+        </div>
+    );
+};
+
+export default Page;
